509a55fe50d8433f7243b4eae6645f8c120b21a7
Juan cruz Leone <juancruzleone@outlook.com>
2024-04-19 11:41:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    name: 'App',
+}
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Inicio</a></li>
+        </ul>
+    </nav>
+
+</template>
