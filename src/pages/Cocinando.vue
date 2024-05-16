ece5d485d3fe8d1f28ac1cf8530f8a13136298e3
Juan cruz Leone <juancruzleone@outlook.com>
2024-05-16 19:40:45 -0300

    Se cambiaron los iconos de categoria en la seccion cocinando

--- src/pages/Cocinando.vue
+++ src/pages/Cocinando.vue
@@ -26,25 +26,25 @@ export default{
             <div class="bg-contenedores w-[200px] h-[180px] radius-categoria m-5">
                 <h3 class="text-white font-montserrat text-center mt-3 cursor-pointer text-xl">Análisis fundamental</h3>
                 <div class="bg-white rounded-full w-[90px] m-auto mt-7 p-5">
-                    <img src="/grafico.svg" alt="Icono de gráfico">
+                    <img src="/analisis.svg" alt="Icono de análisis fundamental">
                 </div>
             </div>
             <div class="bg-contenedores w-[200px] h-[180px] radius-categoria m-5">
                 <h3 class="text-white font-montserrat text-center mt-3 cursor-pointer text-xl">Trading</h3>
                 <div class="bg-white rounded-full w-[90px] m-auto mt-14 p-5">
-                    <img src="/grafico.svg" alt="Icono de gráfico">
+                    <img src="/trading.svg" alt="Icono de trading">
                 </div>
             </div>
             <div class="bg-contenedores w-[200px] h-[180px] radius-categoria m-5">
                 <h3 class="text-white font-montserrat text-center mt-3 cursor-pointer text-xl">Onchain</h3>
                 <div class="bg-white rounded-full w-[90px] m-auto mt-14 p-5">
-                    <img src="/grafico.svg" alt="Icono de gráfico">
+                    <img src="/onchain.svg" alt="Icono de onchain">
                 </div>
             </div>
             <div class="bg-contenedores w-[200px] h-[180px] radius-categoria m-5">
                 <h3 class="text-white font-montserrat text-center mt-3 cursor-pointer text-xl">Airdrops</h3>
                 <div class="bg-white rounded-full w-[90px] m-auto mt-14 p-5">
-                    <img src="/grafico.svg" alt="Icono de gráfico">
+                    <img src="/airdrop.svg" alt="Icono de airdrop">
                 </div>
             </div>
         </div>
