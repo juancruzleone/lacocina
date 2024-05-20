bcae2d98621b7a31f3f7236506b7b64df43611dd
Juan cruz Leone <juancruzleone@outlook.com>
2024-05-20 14:16:05 -0300

    Se agrego img en seccion 404 y se modifico su diseño

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -5,11 +5,12 @@ export default {
 </script>
 
 <template>
-    <div class="container mx-auto text-center mt-28 h-[1400px]">
+    <section class="container mx-auto text-center mt-16 mb-20">
+      <img src="/404.webp" alt="foto de error 404" class="error-404 mx-auto">
       <h1 class="text-6xl font-bold mb-4 font-montserrat">404 - Página no encontrada</h1>
       <p class="text-2xl font-montserrat mb-5">Lo sentimos, la página que estás buscando no existe.</p>
       <router-link to="/" class="text-blue-500 hover:underline bg-black text-white p-2 rounded-lg">Volver al inicio</router-link>
-    </div>
+    </section>
   </template>
   
 
