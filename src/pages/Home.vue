3cfddcb72332d3bd5e0b6013570b4db2883fbfc5
Juan cruz Leone <juancruzleone@outlook.com>
2024-05-29 12:15:57 -0300

    Se hizo responsive la sección del Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -81,58 +81,59 @@ export default {
 
 <template>
   <div>
-    <div class="bg-portada h-[600px] pl-10">
-      <h1 class="font-montserrat font-bold text-7xl pt-[140px]">La cocina</h1>
-      <p class="font-montserrat text-3xl mt-2 pl-1">Comunidad crypto hispana</p>
+    <div class="bg-portada h-[300px] md:h-[400px] lg:h-[600px] pl-5 md:pl-10">
+      <h1 class="font-montserrat font-bold text-4xl md:text-6xl lg:text-7xl pt-[80px] md:pt-[100px] lg:pt-[140px]">La cocina</h1>
+      <p class="font-montserrat text-xl md:text-2xl lg:text-3xl mt-2 pl-1">Comunidad crypto hispana</p>
     </div>
-    <section class="flex mt-20 p-10">
-      <div class="w-[50%] pr-[12%]">
-        <h2 class="font-montserrat font-bold text-7xl justify-center text-center">Quiénes somos?</h2>
-        <div class="flex justify-center m-auto mt-6">
-          <div class="w-20 h-20 rounded-full m-3 border border-gray-900">
+    <section class="flex flex-col lg:flex-row mt-10 md:mt-20 p-5 md:p-10">
+      <div class="lg:w-[50%] lg:pr-[12%] mb-10 lg:mb-0">
+        <h2 class="font-montserrat font-bold text-4xl md:text-6xl lg:text-7xl text-center">Quiénes somos?</h2>
+        <div class="flex justify-center mt-6">
+          <div class="w-16 h-16 md:w-20 md:h-20 rounded-full m-2 md:m-3 border border-gray-900">
             <img src="/nacherx.webp" alt="foto de usuario de nacherx" class="rounded-full">
           </div>
-          <div class="w-20 h-20 rounded-full m-3 border border-gray-900">
+          <div class="w-16 h-16 md:w-20 md:h-20 rounded-full m-2 md:m-3 border border-gray-900">
             <img src="/cromer.webp" alt="foto de usuario de cromer" class="rounded-full">
           </div>
-          <div class="w-20 h-20 rounded-full m-3 border border-gray-900">
+          <div class="w-16 h-16 md:w-20 md:h-20 rounded-full m-2 md:m-3 border border-gray-900">
             <img src="/chefao.webp" alt="foto de usuario de chefao" class="rounded-full">
           </div>
-          <div class="w-20 h-20 rounded-full m-3 border border-gray-900">
+          <div class="w-16 h-16 md:w-20 md:h-20 rounded-full m-2 md:m-3 border border-gray-900">
             <img src="/teos.webp" alt="foto de usuario de teos" class="rounded-full">
           </div>
-          <div class="w-20 h-20 rounded-full m-3 border border-gray-900">
+          <div class="w-16 h-16 md:w-20 md:h-20 rounded-full m-2 md:m-3 border border-gray-900">
             <img src="/kichiro.webp" alt="foto de usuario de kichiro" class="rounded-full">
           </div>
         </div>
       </div>
-      <div class="w-[50%]">
+      <div class="lg:w-[50%]">
         <div>
-          <p class="font-montserrat text-xl w-82">
+          <p class="font-montserrat text-base md:text-lg lg:text-xl">
             Somos una comunidad de criptomonedas hispana que se dedica a brindar contenido educativo, informativo y entretenido sobre el mundo de las criptomonedas.
           </p>
-          <p class="font-montserrat text-xl w-82 mt-2">
-            Nuestro objetivo es proporcionar un espacio donde los entusiastas de las criptomonedas puedan aprender, compartir conocimientos y participar en discusiones sobre diversos temas relacionados con las criptomonedas.</p>
-          <p class="font-montserrat text-xl w-82 mt-2">
+          <p class="font-montserrat text-base md:text-lg lg:text-xl mt-2">
+            Nuestro objetivo es proporcionar un espacio donde los entusiastas de las criptomonedas puedan aprender, compartir conocimientos y participar en discusiones sobre diversos temas relacionados con las criptomonedas.
+          </p>
+          <p class="font-montserrat text-base md:text-lg lg:text-xl mt-2">
             Contamos con 5 expertos, también conocidos como chefs, que publicarán contenido especializado sobre diferentes temas del mundo cripto.
           </p>
         </div>
       </div>
     </section>
-    <section class="pl-10 pt-20 mb-20">
+    <section class="pl-5 md:pl-10 pt-10 md:pt-20 mb-10 md:mb-20">
       <MainH2>Forma parte de nuestra comunidad</MainH2>
-      <div class="flex mt-5 items-center">
-        <div>
-          <img src="/olla.webp" alt="foto de olla" class="w-[150px] h-[150px] mt-5 rounded-full shadow-2xl">
+      <div class="flex flex-col md:flex-row mt-5 items-center">
+        <div class="flex justify-center md:block">
+          <img src="/olla.webp" alt="foto de olla" class="w-[100px] h-[100px] md:w-[150px] md:h-[150px] mt-5 rounded-full shadow-2xl">
         </div>
-        <div class="bg-contenedores w-[87.5%] h-[200px] rounded-lg flex flex-col p-5 shadow-2xl ml-5">
-          <h3 class="text-2xl font-bold mb-2 text-white font-montserrat">Únete a nuestro servidor de Discord</h3>
-          <p class="text-lg mb-4 text-white">Sé parte de una comunidad vibrante y activa donde puedes compartir, aprender y crecer junto a otros entusiastas.</p>
-          <a href="https://discord.gg/Unahg8Vxur" target="_blank" rel="noopener noreferrer" class="p-3 text-black font-montserrat font-semibold bg-white rounded-lg text-center mt-10 w-96">¡Únete Ahora!</a>
+        <div class="bg-contenedores w-full md:w-[87.5%] h-auto md:h-[200px] rounded-lg flex flex-col p-5 shadow-2xl mt-5 md:mt-0 md:ml-5">
+          <h3 class="text-lg md:text-2xl font-bold mb-2 text-white font-montserrat">Únete a nuestro servidor de Discord</h3>
+          <p class="text-base md:text-lg mb-4 text-white">Sé parte de una comunidad vibrante y activa donde puedes compartir, aprender y crecer junto a otros entusiastas.</p>
+          <a href="https://discord.gg/Unahg8Vxur" target="_blank" rel="noopener noreferrer" class="p-3 text-black font-montserrat font-semibold bg-white rounded-lg text-center mt-10 w-full md:w-96">¡Únete Ahora!</a>
         </div>
       </div>
     </section>
-    <section class="p-10 pb-20">
+    <section class="p-5 md:p-10 pb-10 md:pb-20">
       <MainH2>Preguntas frecuentes</MainH2>
       <div class="mt-5">
         <div v-for="(pregunta, index) in preguntas" :key="index" class="mb-3">
@@ -147,15 +148,15 @@ export default {
         </div>
       </div>
     </section>
-    <section class="p-10 pb-20">
+    <section class="p-5 md:p-10 pb-10 md:pb-20">
       <MainH2>Post destacado</MainH2>
       <div class="pt-5">
         <Loader v-if="loading"/>
-        <div v-else class="flex bg-contenedores radius-comunidad p-5 mb-8 overflow-hidden shadow-2xl">
-          <img :src="postDestacado.img1_post" :alt="postDestacado.titulo_post" class="w-60 rounded-lg mb-3 mr-5">
+        <div v-else class="flex flex-col md:flex-row bg-contenedores radius-comunidad p-5 mb-8 overflow-hidden shadow-2xl">
+          <img :src="postDestacado.img1_post" :alt="postDestacado.titulo_post" class="w-full md:w-60 rounded-lg mb-3 md:mb-0 md:mr-5">
           <div>
-              <h3 class="text-white font-montserrat mt-3 cursor-pointer text-2xl font-semibold">{{ postDestacado.titulo_post }}</h3>
-              <p class="font-montserrat bg-white w-[200px] text-center p-1 rounded-lg mt-3 mb-5">{{ postDestacado.categoria_post }}</p>
+              <h3 class="text-white font-montserrat mt-3 cursor-pointer text-xl md:text-2xl font-semibold">{{ postDestacado.titulo_post }}</h3>
+              <p class="font-montserrat bg-white w-[100px] md:w-[200px] text-center p-1 rounded-lg mt-3 mb-5">{{ postDestacado.categoria_post }}</p>
               <p class="text-white mt-2 font-montserrat">{{ postDestacado.descripcion_post }}</p>
               <div class="flex mt-4">
               <router-link :to="'/post/' + postDestacado.id">
@@ -168,4 +169,6 @@ export default {
     </section>
   </div>
 </template>
+
+
 
