958c98f160823d76c16a09b28b76d296e00677d5
Juan cruz Leone <juancruzleone@outlook.com>
2024-05-29 12:36:54 -0300

    Se hizo responsive la sección detalle de posts

--- src/pages/Detalle.vue
+++ src/pages/Detalle.vue
@@ -80,34 +80,32 @@ export default {
 
 <template>
   <div>
-    <div class="bg-portada h-[400px] bg-cover bg-center">
-      <MainH1>{{ post ? post.titulo_post : '' }}</MainH1>
-      <div class="pl-14">
-        <template v-if="post">
-          <p class="font-montserrat text-center bg-black text-white w-[120px] p-2 rounded-lg mt-7">{{ post.categoria_post }}</p>
-        </template>
-      </div>
+    <div class="bg-portada min-h-[200px] md:min-h-[400px] bg-cover bg-center flex flex-col justify-center items-start text-left px-4 md:px-14 py-8 md:py-14">
+      <MainH1 class="text-2xl md:text-5xl pl-0">{{ post ? post.titulo_post : '' }}</MainH1>
+      <template v-if="post">
+        <p class="font-montserrat text-left bg-black text-white w-auto px-4 py-2 rounded-lg mt-4">{{ post.categoria_post }}</p>
+      </template>
     </div>
-    <Loader v-if="!post" class="pl-12"/>
-    <section v-if="post" class="pl-14 pt-10 pb-10">
-      <MainH2 class="border-b-8 border-black">Contenido</MainH2>
+    <Loader v-if="!post" class="pl-5 md:pl-12"/>
+    <section v-if="post" class="px-4 md:px-14 pt-10 pb-10">
+      <MainH2 class="border-b-8 border-black mb-4">Contenido</MainH2>
       <div>
-        <img :src="post.img1_post" alt="Imagen del post" class="w-full h-[400px] rounded-md mt-4 mb-10">
-        <div class="flex font-montserrat">
-          <div class="w-full mr-10">
+        <img :src="post.img1_post" alt="Imagen del post" class="w-full h-48 md:h-96 rounded-md mt-4 mb-10">
+        <div class="flex flex-col md:flex-row font-montserrat">
+          <div class="w-full md:mr-10">
             <h3 class="font-bold text-xl mt-10">{{ post.subtitulo1_post }}</h3>
             <p class="mb-10">{{ post.texto1_post }}</p>
             <h3 class="font-bold text-xl mt-5">{{ post.subtitulo2_post }}</h3>
             <p>{{ post.texto2_post }}</p>
           </div>
-          <div>
-            <img :src="post.img2_post" alt="Imagen del post" class="w-full h-[400px] radius-comunidad mt-4 mb-10">
+          <div class="mt-4 md:mt-0">
+            <img :src="post.img2_post" alt="Imagen del post" class="w-full h-48 md:h-96 rounded-md mt-4 mb-10">
           </div>
         </div>
       </div>
     </section>
-    <section v-if="post" class="pl-14 pb-20">
-      <MainH2 class="border-b-8 border-black">Comentarios</MainH2>
+    <section v-if="post" class="px-4 md:px-14 pb-20">
+      <MainH2 class="border-b-8 border-black mb-4">Comentarios</MainH2>
       <div>
         <Loader v-if="loadingComments" />
         <div v-if="!loadingComments && comments.length === 0" class="font-montserrat font-semibold text-lg mt-2 text-red-500">
@@ -133,9 +131,11 @@ export default {
           <div v-if="user" class="flex items-center justify-between font">
             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Agregar Comentario</button>
           </div>
-          <div v-if="error" class="text-red-500 mb-4 font-montseerat mt-4 text-ls">{{ error }}</div>
+          <div v-if="error" class="text-red-500 mb-4 font-montserrat mt-4 text-ls">{{ error }}</div>
         </form>
       </div>
     </section>
   </div>
 </template>
+
+
