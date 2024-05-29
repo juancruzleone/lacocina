273ad40662cf668f9c74bb334b55731c4fb743b2
Juan cruz Leone <juancruzleone@outlook.com>
2024-05-29 13:03:00 -0300

    Se hizo responsive la sección Cocinando

--- src/pages/Cocinando.vue
+++ src/pages/Cocinando.vue
@@ -52,34 +52,34 @@ export default {
 
 <template>
   <div>
-    <div class="bg-portada h-96">
+    <div class="bg-portada h-64 sm:h-96">
       <MainH1>Cocinando</MainH1>
     </div>
-    <section>
-      <MainH2 class="pl-12 pt-10 pb-5 text-center">Categorias</MainH2>
-      <div class="flex ml-12 justify-center flex-wrap">
-        <div v-for="categoria in categorias" :key="categoria.nombre" class="bg-contenedores w-52 h-44 radius-categoria m-5 flex flex-col items-center justify-center" @click="filtrarPostsPorCategoria(categoria.nombre)">
-          <h3 class="text-white font-montserrat text-center mt-3 cursor-pointer text-xl">{{ categoria.nombre }}</h3>
+    <section class="py-10 sm:py-12 md:py-16">
+      <MainH2 class="px-6 sm:px-12 pb-5 text-center">Categorias</MainH2>
+      <div class="flex flex-wrap justify-center px-6 sm:px-12">
+        <div v-for="categoria in categorias" :key="categoria.nombre" class="bg-contenedores w-52 h-44 radius-categoria m-3 sm:m-5 flex flex-col items-center justify-center cursor-pointer" @click="filtrarPostsPorCategoria(categoria.nombre)">
+          <h3 class="text-white font-montserrat text-center mt-3 text-xl">{{ categoria.nombre }}</h3>
           <div :class="{ 'bg-analisis-fundamental': categoria.nombre === 'Análisis fundamental' }" class="bg-white rounded-full w-24 h-24 flex items-center justify-center mt-10">
             <img :src="categoria.icono" :alt="categoria.nombre" class="w-16">
           </div>
         </div>
       </div>
     </section>
-    <section class="pb-20">
-      <MainH2 class="pl-12 pt-10 pb-5 font-montserrat">Posts recientes</MainH2>
+    <section class="pb-20 sm:pb-24 md:pb-28">
+      <MainH2 class="px-6 sm:px-12 pt-10 pb-5 font-montserrat">Posts recientes</MainH2>
       <!-- Agrega el loader con v-if -->
-      <Loader v-if="loading" class="pl-12"/>
-      <div class="pl-12" v-if="!loading"> <!-- Agrega v-if="!loading" para que el contenido solo se muestre cuando no se esté cargando -->
-        <div v-for="post in postsFiltrados" :key="post.id" class="bg-contenedores w-[98%] radius-comunidad p-5 mb-8 flex">
-          <img :src="post.img1_post" :alt="post.titulo_post" class="w-60 rounded-lg mb-3 mr-5">
+      <Loader v-if="loading" class="px-6 sm:px-12"/>
+      <div class="px-6 sm:px-12" v-if="!loading"> <!-- Agrega v-if="!loading" para que el contenido solo se muestre cuando no se esté cargando -->
+        <div v-for="post in postsFiltrados" :key="post.id" class="bg-contenedores w-full sm:w-[98%] radius-comunidad p-5 mb-8 flex flex-col sm:flex-row">
+          <img :src="post.img1_post" :alt="post.titulo_post" class="w-full sm:w-60 rounded-lg mb-3 sm:mr-5">
           <div>
             <h3 class="text-white font-montserrat mt-3 cursor-pointer text-2xl font-semibold">{{ post.titulo_post }}</h3>
             <p class="font-montserrat bg-white w-[200px] text-center p-1 rounded-lg mt-3 mb-5">{{ post.categoria_post }}</p>
             <p class="text-white mt-2 font-montserrat">{{ post.descripcion_post }}</p>
             <div class="flex mt-4">
               <router-link :to="'/post/' + post.id">
-                <button class="bg-gray-500 text-white rounded-xl  p-2 mr-2">Ver más</button>
+                <button class="bg-gray-500 text-white rounded-xl p-2 mr-2">Ver más</button>
               </router-link>
             </div>
           </div>
